99021ecbfece1463ccb0a59c96aa6f585e1a2f94
DRACKWARS <dionpratama153@gmail.com>
2023-03-24 23:44:30 +0700

    First

---
+++ src/modul3/Dion_Pratama.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Dion_Pratama {
+    public static void main(String[] args){
+        
+     System.out.println("Welcome to Java Programming [Dion Pratama Putra]!!!");
+    }
+}
---
+++ src/modul3/TheTree.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TheTree {
+    public static void main(String[] args){
+        String s1 = "I think that ";
+        String s2 = "I shall never see";
+        String s3 = "a poem as lovely as a tree.";
+        String s4 = "A tree whose hungry mouth is pressed";
+        String s5 = "Against the Earth's sweet flowing breast.";
+        
+        System.out.println("" + s1 + s2);
+        System.out.println("" + s3);
+        System.out.println("" + s4);
+        System.out.println("" + s5); 
+    }
+  
+}
